a14ffb1bf343aee254de7683342a7a1c7e79628e
The-G-Man-Half-Life <mateomoospina@gmail.com>
2024-10-10 08:30:50 -0500

    feat: added code to the program for the webpage initialization

---
+++ Data/ApplicationDbcontext.cs
@@ -0,0 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace GestionDeProductosYServicios.Data;
+public class ApplicationDbcontext:DbContext
+{
+    
+}
--- Program.cs
+++ Program.cs
@@ -1,44 +1,57 @@
+using DotNetEnv;
+using GestionDeProductosYServicios.Data;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Microsoft.EntityFrameworkCore;
+
+
+//cargar las variables de entorno
+
+Env.Load();
+
+var DB_HOST = Environment.GetEnvironmentVariable("DB_HOST");
+var DB_PORT = Environment.GetEnvironmentVariable("DB_PORT");
+var DB_DATABASE = Environment.GetEnvironmentVariable("DB_DATABASE");
+var DB_UID = Environment.GetEnvironmentVariable("DB_UID");
+var DB_PASSWORD = Environment.GetEnvironmentVariable("DB_PASSWORD");
+
+var connectionString =  $"server={DB_HOST};port={DB_PORT};database={DB_DATABASE};uid={DB_UID};password={DB_PASSWORD}";
+
+//Crear builder de la aplicacion web
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//configurar acceso a la base de datos
+builder.Services.AddDbContext<ApplicationDbcontext>(options =>
+    options.UseMySql(connectionString, ServerVersion.Parse("8.0.20-mysql")));
+
+
+//configurando el entorno de la pagina
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>{
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Version 1");
+        options.RoutePrefix = string.Empty;
+    });
 }
 
+app.UseWelcomePage(new WelcomePageOptions{
+    Path = "/"
+});
+
+
+//desarrollar el entorno de la pagina
+app.UseRouting();
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseAuthentication();
+app.UseAuthorization();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
+app.MapControllers();
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
