ccc230895180b5b8785fba3398ce58d7cb85f8bf
The-G-Man-Half-Life <mateomoospina@gmail.com>
2024-10-11 08:35:30 -0500

    fix: fixed the table name for model order

---
+++ Migrations/20241011132923_CreationOfOrderEntity.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestionDeProductosYServicios.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreationOfOrderEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    order_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    order_creation_date = table.Column<DateOnly>(type: "date", nullable: true),
+                    order_delivery_date = table.Column<DateOnly>(type: "date", nullable: true),
+                    client_id = table.Column<int>(type: "int", nullable: false),
+                    carrier_id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.order_id);
+                    table.ForeignKey(
+                        name: "FK_Orders_Carriers_carrier_id",
+                        column: x => x.carrier_id,
+                        principalTable: "Carriers",
+                        principalColumn: "carrier_id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Orders_Clients_client_id",
+                        column: x => x.client_id,
+                        principalTable: "Clients",
+                        principalColumn: "client_id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_carrier_id",
+                table: "Orders",
+                column: "carrier_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_client_id",
+                table: "Orders",
+                column: "client_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
--- Models/Order.cs
+++ Models/Order.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GestionDeProductosYServicios.Models;
 
-[Table("Shipments_Products")]
+[Table("Orders")]
 public class Order
 {
     [Key]
